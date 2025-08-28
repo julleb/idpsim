8959f19b5e7138ace2c43f018a58f3e670a2dbb4
New Name <new.email@example.com>
2025-08-28 20:19:57 +0200

    init spring

---
+++ src/main/java/se/idpsim/Idpsimulator/IdpsimulatorApplication.java
@@ -0,0 +1,13 @@
+package se.idpsim.Idpsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdpsimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdpsimulatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/idpsim/Idpsimulator/IdpsimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package se.idpsim.Idpsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdpsimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
