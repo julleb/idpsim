696f5d1fd98dea7fd53a819338ce84928761ea34
julleb <yo>
2025-08-31 17:59:11 +0200

    fix

--- src/main/java/se/idpsim/Idpsimulator/service/idp/model/SimpleUser.java
+++ src/main/java/se/idpsim/Idpsimulator/service/idp/model/SimpleUser.java
@@ -3,6 +3,7 @@ package se.idpsim.Idpsimulator.service.idp.model;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
+import se.idpsim.Idpsimulator.utils.ObjectUtils;
 
 @Builder
 @Getter
@@ -11,4 +12,13 @@ public final class SimpleUser implements SimulatorUser {
     private String userId;
     private String firstName;
     private String lastName;
+
+    public SimpleUser(String userId, String firstName, String lastName) {
+        ObjectUtils.requireNonNull(userId, "userId cannot be null");
+        ObjectUtils.requireNonNull(firstName, "firstName cannot be null");
+        ObjectUtils.requireNonNull(lastName, "lastName cannot be null");
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
